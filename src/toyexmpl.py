b268ed46452908b2d0d1a6412e283273b030f8a1
markwh <mark.hagemann@gmail.com>
2019-01-21 18:48:54 -0500

    added notebooks, playing with toy uncertainty example

---
+++ src/toyexmpl.py
@@ -0,0 +1,11 @@
+# Using toy example from Brent
+
+import sys
+sys.path.insert(0, "/home/markwh/Documents/RiverObs/src")
+import SWOTRiver as sriver
+
+
+l2file = "../riverobs-data/uncertainty_example/pixel_cloud.nc"
+
+
+
