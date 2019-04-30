c19cf689a7bc8ac2f6132cdfdae0e2d5ccffabf5
Mark Hagemann <mark.hagemann@gmail.com>
2019-04-29 22:09:37 -0400

    modified roruns infrastructure for validation across different characteristics

--- lib/roruns.R
+++ lib/roruns.R
@@ -4,28 +4,55 @@
 
 roruns <- read.csv("src/roruns.csv", stringsAsFactors = FALSE)
 
+ro_manifest <- function(files = paste0("~/Documents/swot-error/src/",
+                                       c("roruns.csv", "roruns37.csv"))) {
+  runnames <- gsub("\\..+$", "", gsub("^.*(.*/)+", "", files))
+  out <- purrr::map(files, ~read.csv(., stringsAsFactors = FALSE)) %>% 
+    setNames(runnames) %>% 
+    bind_rows(.id = "runset")
+}
+
 #' Find roruns that are similar to a given rorun.
 #' 
-#' @param rorow Row number in roruns.csv to reference
+#' @param runno Run number in roruns manifest to reference
 #' @param vary vector of roruns.csv columns that are allowed to vary; 
 #'   other (relevant) columns will match rorow exactly.
-romatch <- function(rorow, vary) {
-  relcols <- c("priordb", "case", "pass", "bndry_cond", "smearing",
-               "land_sig0", "water_sig0", "gdem_name")
+romatch <- function(runno, vary, manifest = ro_manifest()) {
+  rorow <- match(runno, manifest$outno)
+  relcols <- c("priordb", "case", "pass", "day", "smearing",
+               "land_sig0", "water_sig0", "gdem_name", "refdem",
+               "refdem_res")
   keepcols <- setdiff(relcols, vary)
-  rowstrings <- apply(roruns[, keepcols], 1, paste0, collapse = "")
-  matchrow <- paste0(roruns[rorow, keepcols], collapse = "")
-  out <- setdiff(which(matchrow == rowstrings), rorow)
+  rowstrings <- apply(manifest[, keepcols], 1, paste0, collapse = "")
+  matchrow <- paste0(manifest[rorow, keepcols], collapse = "")
+  outrows <- setdiff(which(matchrow == rowstrings), rorow)
+  out <- manifest$outno[outrows]
   out
 }
 
 #' Fetch the directory path for a given rorun, specified as a row number of roruns.csv.
 #' 
-#' @param rorow Row number in roruns.csv to reference
-rodir <- function(rorow, 
+#' @param runno Run number in roruns manifest to reference
+#' @param manifest as returned by \code{ro_manifest()}
+rodir <- function(runno, manifest = ro_manifest(),
                   basedir = getOption("ro_basedir", 
                                       "~/Documents/swot-error/")) {
-  out <- normalizePath(fs::path(basedir, roruns$outdir[rorow]))
+  rorow <- match(runno, manifest$outno)
+  out <- normalizePath(fs::path(basedir, manifest$outdir[rorow]))
   names(out) <- roruns$outno[rorow]
+  out
+}
+
+
+#' Get a bound data.frame for multiple runs. 
+#' 
+rt_valdata_multi <- function(runnos, manifest = ro_manifest(),
+                             basedir = getOption("ro_basedir")) {
+  valdfs <- purrr::map(runnos, ~rt_valdata(rodir(., manifest = manifest)),
+                       basedir = basedir) %>% 
+    setNames(runnos)
+  out <- bind_rows(valdfs, .id = "run") %>% 
+    mutate(run = as.numeric(run)) %>% 
+    left_join(manifest, by = c(run = "outno"))
   out
 }
